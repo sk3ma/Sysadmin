d23762a8eb8dd9801ce31b2ae4dbfafbff837920
sk3ma <52137438+sk3ma@users.noreply.github.com>
2022-02-08 15:27:32 +0200

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -6,19 +6,19 @@
 ##############################################################################
 
 # Declaring variable.
-#USERID=$(id -u)
+USERID=$(id -u)
 
 # Sanity checking.
-#if [[ ${USERID} -ne "0" ]]; then
-#    echo -e "\e[1;3mYou must be root, exiting.\e[m"
-#    exit 1
-#fi
+if [[ ${USERID} -ne "0" ]]; then
+    echo -e "\e[1;3mYou must be root, exiting.\e[m"
+    exit 1
+fi
 
 # Java installation.
 java() {
     echo -e "\e[1;3mInstalling Java\e[m"
-    sudo apt update
-    sudo apt install openjdk-11-jdk -qy
+    apt update
+    apt install openjdk-11-jdk -qy
 }
 
 # Jenkins installation.
@@ -26,20 +26,21 @@ jenkins() {
     echo -e "\e[1;3mAdding repository\e[m"
     cd /opt
     wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-    sudo bash -c 'echo "deb https://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list'
+    bash -c 'echo "deb https://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list'
     echo -e "\e[1;3mInstalling Jenkins\e[m"
-    sudo apt update
-    sudo apt install jenkins -qy
+    apt update
+    apt install jenkins -qy
 }
 
 # Maven installation.
 maven() {
     echo -e "\e[1;3mInstalling Maven\e[m"
-    sudo mkdir -v maven
+    mkdir -v maven
     cd maven
-    sudo wget --progress=bar:force https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
-    sudo tar -xvzf apache-maven-3.8.4-bin.tar.gz
-    sudo rm -rf apache-maven-3.8.4-bin.tar.gz
+    wget --progress=bar:force https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
+    tar -xvzf apache-maven-3.8.4-bin.tar.gz
+    mv -v apache-maven-3.8.4 maven
+    rm -rf apache-maven-3.8.4-bin.tar.gz
 }
 
 # Maven configuration.
@@ -47,28 +48,30 @@ environment() {
     echo -e "\e[1;3mPreparing environment\e[m"
     cp -v ~/.profile ~/.profile.orig
     echo 'export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64' >> ~/.profile
-    echo 'export M2_HOME=/opt/apache-maven-3.8.4' >> ~/.profile
+    echo 'export M2_HOME=/opt/maven' >> ~/.profile
+    echo 'export M2=/opt/maven/bin' >> ~/.profile
     echo 'export PATH=${M2_HOME}/bin:${PATH}' >> ~/.profile
+    echo 'export PATH=${PATH}:${HOME}/bin:${JAVA_HOME}' >> ~/.profile
 }
 
 # Creating exception.
 firewall() {
     echo -e "\e[1;3mAdjusting firewall\e[m"
-    sudo sed -ie 's/HTTP_PORT=8080/HTTP_PORT=9090/g' /etc/default/jenkins
-    sudo ufw allow 9090/tcp
-    echo "y" | sudo ufw enable
-    sudo ufw reload
+    sed -ie 's/HTTP_PORT=8080/HTTP_PORT=8090/g' /etc/default/jenkins
+    ufw allow 8090/tcp
+    echo "y" | ufw enable
+    ufw reload
 }
 
 # Enabling service.
 service() {
     echo -e "\e[1;3mStarting Jenkins\e[m"
-    sudo systemctl restart jenkins
-    sudo systemctl enable jenkins
-#    sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+    systemctl restart jenkins
+    systemctl enable jenkins
+    cat /var/lib/jenkins/secrets/initialAdminPassword
 #    echo -e "\e[1;3;5mExecuting Ansible script...\e[m"
 #    source /vagrant/Ansible.sh
-#    exit
+    exit
 }
 
 # Calling functions.
