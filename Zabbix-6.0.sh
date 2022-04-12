fdb981e4e52c6a007921a214586b580d287c7e76
sk3ma <52137438+sk3ma@users.noreply.github.com>
2022-04-12 11:17:49 +0200

    Update Zabbix-6.0.sh

--- Zabbix-6.0.sh
+++ Zabbix-6.0.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-######################################################################
-# This script will automate a Zabbix installation on Ubuntu 20.04.   #
-# The script installs the LAMP stack and downloads Zabbix server 6.  #
-######################################################################
+#####################################################################
+# This script will automate a Zabbix installation on Ubuntu 20.04.  #
+# The script installs the LAMP stack and downloads Zabbix server 6. #
+#####################################################################
 
 # Declaring variables.
 DISTRO=$(lsb_release -ds)
