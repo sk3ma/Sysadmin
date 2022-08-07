4fca49d22a935f2b7da5e92a9631bca918bcbe5c
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2022-08-07 12:29:40 +0200

    Update Geonode_2.sh

--- Geonode_2.sh
+++ Geonode_2.sh
@@ -13,13 +13,25 @@ geonode() {
     echo -e "\e[32;1;3mCreating containers\e[m"
     docker-compose -f docker-compose.yml pull
     docker-compose -f docker-compose.yml up -d
+}
+
+# Configuring environment.
+environment() {
+    echo -e "\e[32;1;3mUpdating configuration\e[m"
+    sudo sed -i 's|SITEURL=http://localhost/|SITEURL=http://192.168.56.80/|g' .env
+    sudo sed -i 's|GEOSERVER_WEB_UI_LOCATION=http://localhost/geoserver/|GEOSERVER_WEB_UI_LOCATION=http://192.168.56.80/geoserver/|g' .env
+    sudo sed -i 's|GEOSERVER_PUBLIC_LOCATION=http://localhost/geoserver/|GEOSERVER_PUBLIC_LOCATION=http://192.168.56.80/geoserver/|g' .env
+    sudo sed -i 's|GEONODE_LB_HOST_IP=localhost|GEONODE_LB_HOST_IP=192.168.56.80|g' .env
+    echo -e "\e[32;1;3mRestarting containers\e[m"
+    docker-compose up -d
     echo -e "\e[32;1;3mShowing containers\e[m"
     docker-compose ps
     echo -e "\e[33;1;3;5mFinished, configure webUI.\e[m"
     exit
 }
 
-# Calling function.
+# Calling functions.
 if [[ -f /etc/lsb-release ]]; then
     geonode
+    environment
 fi
