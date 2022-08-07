9179724577afabd3b9e73e77f8bfa5a68a77069b
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2022-08-07 14:33:16 +0200

    Update Geonode_2.sh

--- Geonode_2.sh
+++ Geonode_2.sh
@@ -5,9 +5,6 @@
 # Part two will prepare Geonode, the containers, and the environment #
 ######################################################################
 
-# Declaring variable.
-IPADDR=192.168.56.80
-
 # Geonode installation.
 geonode() {
     echo -e "\e[32;1;3mDownloading Geonode\e[m"
@@ -21,10 +18,12 @@ geonode() {
 # Configuring environment.
 environment() {
     echo -e "\e[32;1;3mUpdating configuration\e[m"
-    sudo sed -i 's|SITEURL=http://localhost/|SITEURL=http://${IPADDR}/|g' .env
-    sudo sed -i 's|GEOSERVER_WEB_UI_LOCATION=http://localhost/geoserver/|GEOSERVER_WEB_UI_LOCATION=http://${IPADDR}/geoserver/|g' .env
-    sudo sed -i 's|GEOSERVER_PUBLIC_LOCATION=http://localhost/geoserver/|GEOSERVER_PUBLIC_LOCATION=http://${IPADDR}/geoserver/|g' .env
-    sudo sed -i 's|GEONODE_LB_HOST_IP=localhost|GEONODE_LB_HOST_IP=${IPADDR}|g' .env
+    sudo sed -i 's|HTTP_HOST=localhost|HTTP_HOST=http://192.168.56.80|g' .env
+    sudo sed -i 's|SITEURL=http://localhost/|SITEURL=http://192.168.56.80/|g' .env
+    sudo sed -i 's|GEOSERVER_WEB_UI_LOCATION=http://localhost/geoserver/|GEOSERVER_WEB_UI_LOCATION=http://192.168.56.80/geoserver/|g' .env
+    sudo sed -i 's|GEOSERVER_PUBLIC_LOCATION=http://localhost/geoserver/|GEOSERVER_PUBLIC_LOCATION=http://192.168.56.80/geoserver/|g' .env
+    sudo sed -i 's|GEOSERVER_PUBLIC_LOCATION=http://localhost/geoserver/|GEOSERVER_PUBLIC_LOCATION=http://192.168.56.80/geoserver/|g' .env
+    sudo sed -i 's|GEONODE_LB_HOST_IP=localhost|GEONODE_LB_HOST_IP=192.168.56.80|g' .env
     echo -e "\e[32;1;3mRestarting containers\e[m"
     docker-compose up -d
     echo -e "\e[32;1;3mShowing containers\e[m"
