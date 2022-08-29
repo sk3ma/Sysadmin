a82bacedb032d3cb47a1b7136dace70e4e15347c
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2022-08-29 10:23:10 +0200

    Update Graylog.sh

--- Graylog.sh
+++ Graylog.sh
@@ -2,14 +2,16 @@
 
 ##############################################################################
 # The purpose of the script is to automate a Graylog installation on Ubuntu. #
-# The script installs Java 11, Elasticsearch, MongoDB and Graylog server.    #
+# The script installs Java 11, Elasticsearch, MongoDB and Graylog server 4.  #
 ##############################################################################
 
 # Declaring variables.
 DISTRO=$(lsb_release -ds)
 USERID=$(id -u)
 ROOTPASS=$(echo -n P@ssword321 | sha256sum | cut -d" " -f1)
-IPADDR=192.168.56.75
+SECRET=$(pwgen -N 1 -s 96)
+IPADDR=192.168.56.70
+GUSER="osadmin"
 EMAIL="sk3ma87@gmail.com"
 EPORT=9200
 GPORT=9000
@@ -26,53 +28,30 @@ java() {
     echo -e "\e[32;1;3mUpdating repositories\e[m"
     apt update
     echo -e "\e[32;1;3mInstalling Java\e[m"
-    apt install openjdk-11-jdk openjdk-11-jre uuid-runtime -qy
+    apt install openjdk-11-jdk openjdk-11-jre-headless uuid-runtime -qy
 }
 
 # MongoDB installation.
 mongo() {
     echo -e "\e[32;1;3mInstalling MongoDB\e[m"
-    apt install apt-transport-https ca-certificates dirmngr gnupg software-properties-common -qy
-    wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
-    add-apt-repository 'deb [arch=amd64] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse' -y
-    apt install mongodb-org -qy
-}
-
-# Service file.
-unit() {
-    echo -e "\e[32;1;3mCreating service\e[m"
-    local unit=$(cat << STOP
-[Unit]
-Description=MongoDB database server
-After=network.target
-[Service]
-User=mongodb
-Group=mongodb
-ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
-PIDFile=/var/run/mongodb/mongod.pid
-[Install]
-WantedBy=multi-user.target
-STOP
-)
-    echo "${unit}" > /etc/systemd/system/mongodb.service
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+    echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+    apt update && apt install mongodb-org -qy
+    echo -e "\e[32;1;3mRestarting service\e[m"
     systemctl daemon-reload
-    echo -e "\e[32;1;3mStarting MongoDB\e[m"
-    systemctl enable --now mongodb
-    systemctl start mongodb
+    systemctl enable --now mongod && systemctl restart mongod
 }
 
 # Elasticsearch installation.
 elastic() {
-    echo -e "\e[32;1;3mAdding repository\e[m"
+    echo -e "\e[32;1;3mInstalling Elasticsearch\e[m"
     wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
     echo "deb https://artifacts.elastic.co/packages/oss-7.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-7.x.list
-    echo -e "\e[32;1;3mInstalling Elasticsearch\e[m"
     apt update && apt install elasticsearch-oss -qy
     echo -e "\e[32;1;3mConfiguring Elasticsearch\e[m"
-    cp -v /etc/elasticsearch/elasticsearch.{yml,orig}
-    rm -f /etc/elasticsearch/elasticsearch.yml
+    cd /etc/elasticsearch
+    cp -v elasticsearch.{yml,orig}; rm -f elasticsearch.yml
     local elastic=$(cat << STOP
-# Custom configuration.
 node:
   name: "Elasticsearch"
 path:
@@ -80,68 +59,35 @@ path:
   logs: /var/log/elasticsearch
 cluster:
   name: graylog
-network:
-  host: ${IPADDR}
-  bind_host: 127.0.0.1
-http:
-  port: ${EPORT}
-discovery:
-  seed_hosts: [0.0.0.0]
 action.auto_create_index: false
 STOP
 )
-    echo "${elastic}" > /etc/elasticsearch/elasticsearch.yml
-    echo -e "\e[32;1;3mResizing Heap\e[m"
-    sed -i 's|-Xms1g|-Xms2g|g' /etc/elasticsearch/jvm.options
-    sed -i 's|-Xmx1g|-Xmx2g|g' /etc/elasticsearch/jvm.options
-    echo -e "\e[32;1;3mElasticsearch options\e[m"
-    local config=$(cat << STOP
-# Elasticsearch configuration.
-ES_PATH_CONF=/etc/elasticsearch
-ES_STARTUP_SLEEP_TIME=5
-ES_USER=elasticsearch
-ES_GROUP=elasticsearch
-LOG_DIR=/var/log/elasticsearch
-DATA_DIR=/var/lib/elasticsearch
-WORK_DIR=/tmp/elasticsearch
-CONF_DIR=/etc/elasticsearch
-CONF_FILE=/etc/elasticsearch/elasticsearch.yml
-RESTART_ON_UPGRADE=true
-START_DAEMON=true
-STOP
-)
-    echo "${config}" > /etc/default/elasticsearch
-    chown -R elasticsearch:elasticsearch /var/lib/elasticsearch/
-    echo -e "\e[32;1;3mRestarting Elasticsearch\e[m"
-    systemctl restart elasticsearch
+    echo "${elastic}" > elasticsearch.yml
+    sed -i 's|-Xms1g|-Xms2g|g' jvm.options
+    sed -i 's|-Xmx1g|-Xmx2g|g' jvm.options
+    echo -e "\e[32;1;3mRestarting service\e[m"
+    systemctl daemon-reload
+    systemctl enable --now elasticsearch && systemctl restart elasticsearch
 }
 
 # Graylog installation.
-graylog() {
+server() {
     echo -e "\e[32;1;3mInstalling Graylog\e[m"
     cd /opt
-    wget --progress=bar:force https://packages.graylog2.org/repo/packages/graylog-4.2-repository_latest.deb
-    dpkg -i graylog-4.2-repository_latest.deb
-    apt update && apt install graylog-server pwgen -qy
-    echo -e "\e[32;1;3mStarting Graylog\e[m"
-    systemctl start graylog-server
-    systemctl enable graylog-server
-    rm -f graylog-4.2-repository_latest.deb
-}
-
-# Graylog configuration.
-config() {
+    wget --progress=bar:force https://packages.graylog2.org/repo/packages/graylog-4.3-repository_latest.deb
+    dpkg -i graylog-4.3-repository_latest.deb
+    apt update && apt install graylog-server graylog-enterprise-plugins graylog-integrations-plugins graylog-enterprise-integrations-plugins pwgen -qy
+    echo -e "\e[32;1;3mRestarting service\e[m"
+    systemctl daemon-reload
+    systemctl enable --now graylog-server && systemctl start graylog-server
     echo -e "\e[32;1;3mConfiguring Graylog\e[m"
-    local secret=$(pwgen -N 1 -s 96)
     cd /etc/graylog/server
-    cp -v server.{conf,orig}
-    rm -f server.conf
+    cp -v server.{conf,orig}; rm -f server.conf
     tee server.conf << STOP
-# Graylog server configuration.
 is_master = true
 node_id_file = /etc/graylog/server/node-id
-password_secret = ${secret}
-root_username = osadmin
+password_secret = ${SECRET}
+root_username = ${GUSER}
 root_password_sha2 = ${ROOTPASS}
 root_email = ${EMAIL}
 bin_dir = /usr/share/graylog-server/bin
@@ -177,44 +123,40 @@ mongodb_max_connections = 1000
 mongodb_threads_allowed_to_block_multiplier = 5
 proxied_requests_thread_pool_size = 32
 STOP
-    echo -e "\e[32;1;3mRestarting Graylog\e[m"
     systemctl restart graylog-server
+    rm -rf /opt/graylog-4.3-repository_latest.deb
 }
 
 # Syslog configuration.
-syslog() {
-    echo -e "\e[32;1;3mConfiguring Syslog\e[m"
-    echo -e 'module(load="imudp")' >> /etc/rsyslog.conf
-    echo -e 'input(type="imudp" port="514")' >> /etc/rsyslog.conf
-    echo -e 'module(load="imtcp")' >> /etc/rsyslog.conf
-    echo -e 'input(type="imtcp" port="514")' >> /etc/rsyslog.conf
-    echo -e "*.*@127.0.0.1:5140" >> /etc/rsyslog.conf
-    echo -e "\e[32;1;3mRestarting Syslog\e[m"
+gray() {
+    echo -e "\e[32;1;3mConfiguring syslog\e[m"
+    echo -e 'module(load="imudp")' >> /etc/rsyslog.d/graylog.conf
+    echo -e 'input(type="imudp" port="514")' >> /etc/rsyslog.d/graylog.conf
+    echo -e 'module(load="imtcp")' >> /etc/rsyslog.d/graylog.conf
+    echo -e 'input(type="imtcp" port="514")' >> /etc/rsyslog.d/graylog.conf
+    echo -e "*.*@127.0.0.1:5140:RSYSLOG_SyslogProtocol23Format" >> /etc/rsyslog.d/graylog.conf
     systemctl restart rsyslog
+    logger "Sample: Testing log file."
 }
 
 # Creating exception.
-firewall() {
-    echo -e "\e[32;1;3mAdjusting firewall\e[m"
+fire() {
+    ufw allow 5140/tcp
     ufw allow 9000/tcp
     ufw allow 9200/tcp
     echo "y" | ufw enable
     ufw reload
-    echo -e "\e[32;1;3mTesting Elasticsearch\e[m"
-    curl -X GET "http://${IPADDR}:${GPORT}"
-    echo -e "\e[33;1;3;5mFinished, configure webUI.\e[m"
+    echo -e "\e[33;1;3;5mFinished, installation complete.\e[m"
     exit
 }
 
 # Calling functions.
 if [[ -f /etc/lsb-release ]]; then
-    echo -e "\e[33;1;3;5mUbuntu detected, proceeding...\e[m"
+    echo -e "\e[35;1;3;5mUbuntu detected, proceeding...\e[m"
     java
     mongo
-    unit
     elastic
-    graylog
-    config
-    syslog
-    firewall
+    server
+    gray
+    fire
 fi
