2abb5ee49164bd3c7cb5c30a993572fdc2fdd8c2
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2022-10-04 15:55:17 +0200

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -41,6 +41,7 @@ jenkins() {
     sed -ie 's|HTTP_PORT=8080|HTTP_PORT=8090|g' /etc/default/jenkins
     sed -ie 's|JENKINS_ARGS="--webroot=/var/cache/$NAME/war --httpPort=$HTTP_PORT"|JENKINS_ARGS="--webroot=/var/cache/$NAME/war --httpPort=$HTTP_PORT --httpListenAddress=192.168.56.72"|g' /etc/default/jenkins
     sed -ie 's|Environment="JENKINS_PORT=8080"|Environment="JENKINS_PORT=8090"|g' /usr/lib/systemd/system/jenkins.service
+    systemctl daemon-reload
 }
 
 # Maven installation.
