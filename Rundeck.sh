d7458ece85aa8772958efcd6a6b4cef0484530d7
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-07-20 09:31:09 +0200

    Update Rundeck.sh

--- Rundeck.sh
+++ Rundeck.sh
@@ -95,12 +95,12 @@ config() {
 }
 
 fire() {
-    echo -e "\e[32;1;3mAdjusting firewall\e[m"
+    echo -e "\e[32;1;3m[INFO] Adjusting firewall\e[m"
     ufw allow 80,443/tcp
     ufw allow 4440/tcp
     echo "y" | ufw enable
     ufw reload
-    echo -e "\e[33;1;3;5mFinished, installation complete.\e[m"
+    echo -e "\e[33;1;3;5m[INFO] Finished, installation complete.\e[m"
     exit
 }
 
