d2132954e7ceb0e1a22a613033af48e03d593746
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-08-12 16:13:48 +0200

    Update Geonode_2.sh

--- Geonode_2.sh
+++ Geonode_2.sh
@@ -7,17 +7,17 @@
 
 # Geonode installation.
 geonode() {
-    echo -e "\e[32;1;3mDownloading Geonode\e[m"
+    echo -e "\e[32;1;3m[INFO] Downloading Geonode\e[m"
     git clone https://github.com/GeoNode/geonode.git -b 3.2.x /opt/geonode
     cd /opt/geonode
-    echo -e "\e[32;1;3mCreating containers\e[m"
+    echo -e "\e[32;1;3m[INFO] Creating containers\e[m"
     docker-compose -f docker-compose.yml pull
     docker-compose -f docker-compose.yml up -d
 }
 
 # Configuring environment.
 environment() {
-    echo -e "\e[32;1;3mUpdating configuration\e[m"
+    echo -e "\e[32;1;3m[INFO] Updating configuration\e[m"
     sudo sed -i 's|HTTP_HOST=localhost|HTTP_HOST=192.168.56.80|g' .env
     sudo sed -i 's|GEONODE_LB_HOST_IP=localhost|GEONODE_LB_HOST_IP=192.168.56.80|g' .env
     sudo sed -i 's|SITEURL=http://localhost/|SITEURL=http://192.168.56.80/|g' .env
@@ -26,14 +26,20 @@ environment() {
     sudo sed -i 's|GEOSERVER_PUBLIC_LOCATION=http://localhost/geoserver/|GEOSERVER_PUBLIC_LOCATION=http://192.168.56.80/geoserver/|g' .env
     echo -e "\e[32;1;3mRestarting containers\e[m"
     docker-compose up -d
-    echo -e "\e[32;1;3mShowing containers\e[m"
+    echo -e "\e[32;1;3m[INFO] Showing containers\e[m"
     docker-compose ps
-    echo -e "\e[33;1;3;5mFinished, configure webUI.\e[m"
+    echo -e "\e[33;1;3;5m[✓] Finished, configure webUI.\e[m"
     exit
 }
 
-# Calling functions.
-if [[ -f /etc/lsb-release ]]; then
+# Defining function.
+main() {
     geonode
     environment
+}
+
+# Calling function.
+if [[ -f /etc/lsb-release ]]; then
+    echo -e "\e[35;1;3;5m[OK] Ubuntu detected, proceeding...\e[m"
+    main
 fi
