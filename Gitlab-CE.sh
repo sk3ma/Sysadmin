46579af42c6eda32c33e96b76e4e32217b2f5ac3
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-08-19 22:17:07 +0200

    Update Gitlab-CE.sh

--- Gitlab-CE.sh
+++ Gitlab-CE.sh
@@ -16,6 +16,7 @@ echo -e "\e[96;1;3m[OK] Distribution: ${DISTRO}\e[m"
 sudo apt update
 sudo apt install curl openssh-server ca-certificates -qy
 echo -e "\e[32;1;3m[INFO] Downloading Gitlab\e[m"
+cd /tmp
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 
 # Installing Gitlab
