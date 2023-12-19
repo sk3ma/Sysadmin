b4dad97f289a97e6518c37fabc6c2f15b13d42b9
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2023-12-19 09:55:37 +0200

    Updated file

--- Jenkins.sh
+++ Jenkins.sh
@@ -135,7 +135,6 @@ service() {
  __|  |___ ___| |_|_|___ ___ 
 |  |  | -_|   | '_| |   |_ -|
 |_____|___|_|_|_,_|_|_|_|___|
-                             
                             "
     systemctl restart jenkins
     systemctl enable jenkins
